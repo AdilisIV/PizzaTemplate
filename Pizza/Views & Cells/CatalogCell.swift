1c124ebb2d4a1c3496aa1ccc98f886d500f4d527
Alexander Kosse <work@vl2003.ar14-24.ekb.ru.mirasystem.net>
2017-11-14 16:29:29 +0500

    !

---
+++ Pizza/Views & Cells/CatalogCell.swift
@@ -0,0 +1,13 @@
+//
+//  CatalogCell.swift
+//  Pizza
+//
+//  Created by Alexander Kosse on 14/11/2017.
+//  Copyright © 2017 Information Technologies, LLC. All rights reserved.
+//
+
+import UIKit
+
+class CatalogCell: UICollectionViewCell {
+    
+}
