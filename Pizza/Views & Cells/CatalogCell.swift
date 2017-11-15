9cb24fe68dfeb6ebef4c3c6b000baee7c45b566e
Alexander Kosse <work@vl2003.ar14-24.ekb.ru.mirasystem.net>
2017-11-15 09:26:53 +0500

    !

--- Pizza/Views & Cells/CatalogCell.swift
+++ Pizza/Views & Cells/CatalogCell.swift
@@ -10,4 +10,18 @@ import UIKit
 
 class CatalogCell: UICollectionViewCell {
     
+    override func awakeFromNib() {
+        
+        self.contentView.layer.cornerRadius = 2.0
+        self.contentView.layer.borderWidth = 1.0
+        self.contentView.layer.borderColor = UIColor.clear.cgColor
+        self.contentView.layer.masksToBounds = true
+        
+        self.layer.shadowColor = UIColor.lightGray.cgColor
+        self.layer.shadowOffset = CGSize(width: 0, height: 2.0)
+        self.layer.shadowRadius = 2.0
+        self.layer.shadowOpacity = 1.0
+        self.layer.masksToBounds = false
+//        self.layer.shadowPath = UIBezierPath(roundedRect: self.bounds, cornerRadius: self.contentView.layer.cornerRadius).cgPath
+    }
 }
